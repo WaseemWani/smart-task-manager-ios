a2b640ae52c5d65a2b6efe66acf68edf9a2ed9b6
Waseem Wani <WaseemWani23@Waseems-MacBook-Pro.local>
2026-07-01 12:25:01 +0530

    initial project setup

---
+++ SmartTaskManager/SmartTaskManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartTaskManager
+//
+//  Created by Waseem Wani on 01/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
